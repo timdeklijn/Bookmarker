bfd9358c29c0e55bfb7968a3502aaf98b385d445
Tim de Klijn <timdeklijn@gmail.com>
2023-08-28 16:38:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Bookmarker
+
+![image]({https://img.shields.io/badge/Shell_Script-121011?style=for-the-badge&logo=gnu-bash&logoColor=white})
+
+A simple bash script to take descriptors + URLs from a file, use `fzf` to filter
+and open the URL as a new tab in `firefox`.
+
+## Installation
+
+Make sure [fzf](https://github.com/junegunn/fzf) is installed.
+
+Make the script executable and link it to a local bin folder. Create a file
+called `.links.txt` in the user root folder (`~/.links.txt`). The script
+can be called using `bookmarker`, and a `fzf` window should appear.
+
+## Usage
+
+Write URLs into the links file in the following format:
+
+``` text
+name:    https://www.URL.com
+```
+
+The formatting of the file should follow these rules:
+
+- The white space can be tabs or spaces,
+- The URL should **alwais** start with `http://`.
---
+++ bookmarker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# bookmarker script. Use a specified file that contains a description and a
+# link. This will be piped into fzf which filters it to a single line. The url
+# on the line is passed to firefox which opens the url on a new tab.
+
+# TODO: create the links file if it does not exist.
+#
+# from the text in the "links" file, use the first column as search in fzf.
+# Thene give back the second column, place this in the `url` variable.
+url=$(echo "$(cat ~/.links.txt)" | \
+  fzf --color 16 | \
+  awk '{print $NF}' \
+)
+
+# Early exit, if url is not set, exit the script and DO NOT open firefox.
+if [ "$url" = "" ]; then
+  echo "no choice made, exiting"
+  exit 0
+fi
+
+
+# TODO: switch firefox call based on current OS. Currently this is only working
+#       on MacOS, want to make it work on Linux too
+open -a /Applications/Firefox.app $url
